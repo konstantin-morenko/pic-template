0c5df9c36d5a991f7eff1721278474e8782c0a97
Konstantin Morenko <me@konstantin-morenko.ru>
2020-03-07 15:52:56 +0300

    pragma config example

--- src/confbits.c
+++ src/confbits.c
@@ -3,10 +3,28 @@
 
 /* ==================== CONFIG ==================== */
 
-#error Config in __FILE__ is not set
+/* /opt/microchip/xc8/v2.10/docs/chips/16f628a.html */
+#pragma config FOSC = INTOSCIO  // INTOSC: I/O function on RA6/RA7
+#pragma config WDTE = OFF       // WDT disabled
+#pragma config PWRTE = OFF      // PWRT disabled
+#pragma config MCLRE = ON       // RA5/MCLR/VPP pin function is MCLR
+#pragma config BOREN = OFF      // BOD disabled
+#pragma config LVP = OFF        // RB4/PGM pin has digital I/O function, HV on MCLR
+#pragma config CPD = OFF        // Data memory code protection off
+#pragma config CP = OFF         // Code protection off
+
+// #error Config in __FILE__ is not set
 /* Revome this after configuring */
 
 /* ==================== IDLOC ==================== */
 
-#error IdLoc in __FILE__ is not set
+/* Rules for setting my IdLoc */
+/* https://github.com/konstantin-morenko/my-pic-idloc */
+
+#pragma config IDLOC0 = 0
+#pragma config IDLOC1 = 0
+#pragma config IDLOC2 = 0
+#pragma config IDLOC3 = 0
+
+// #error IdLoc in __FILE__ is not set
 /* Revome this after configuring */
